96291b4b56ee804d7859ca6c28e3a7c5b7f39a19
Pete Doherty <ethagnawl@gmail.com>
2012-04-02 14:00:50 -0400

    updating specs

--- spec/hashy.spec.coffee
+++ spec/hashy.spec.coffee
@@ -2,30 +2,45 @@ describe 'hashy', ->
     beforeEach -> window.location.hash = ''
     afterEach -> window.location.hash = ''
 
-    it 'creates a new hash containing a single k,v pair', ->
+    it 'gets the hash', ->
+        window.location.hash = 'this_is_a_hash'
+        result = hashy()
+        expect(result).toEqual('#this_is_a_hash')
+
+    it 'sets the hash', ->
+        hashy('this_is_a_hash')
+        result = hashy()
+        expect(result).toEqual('#this_is_a_hash')
+
+    it 'sets an empty hash with one k,v pair', ->
         hashy('key', 'value')
-        result = window.location.hash
+        result = hashy()
         expect(result).toEqual('#key=value')
+
+    it 'sets an empty hash with multiple k,v pairs', ->
+        hashy(['some', 'pair'], ['key', 'value'], ['test', 'cool'])
+        result = hashy()
+        expect(result).toEqual('#some=pair&key=value&test=cool')
 
     it 'updates an existing k,v pair in the hash', ->
         hashy('key', 'value')
         hashy('key', 'upated_value')
-        result = window.location.hash
+        result = hashy()
         expect(result).toEqual('#key=upated_value')
 
     it 'adds a k,v pair to an existing hash', ->
         hashy('key', 'value')
         hashy('key_1', 'value')
-        result = window.location.hash
+        result = hashy()
         expect(result).toEqual('#key=value&key_1=value')
 
     it 'updates an existing k,v pair within a hash containing multiple k,v pairs', ->
         hashy(['some', 'pair'], ['key', 'value'], ['test', 'cool'])
         hashy('key', 'upated_value')
-        result = window.location.hash
+        result = hashy()
         expect(result).toEqual('#some=pair&key=upated_value&test=cool')
 
     it 'creates a new hash containing multiple k,v pairs', ->
         hashy(['some', 'pair'], ['key', 'value'], ['test', 'cool'])
-        result = window.location.hash
+        result = hashy()
         expect(result).toEqual('#some=pair&key=value&test=cool')
--- spec/hashy.spec.js
+++ spec/hashy.spec.js
@@ -6,37 +6,55 @@
     afterEach(function() {
       return window.location.hash = '';
     });
-    it('creates a new hash containing a single k,v pair', function() {
+    it('gets the hash', function() {
+      var result;
+      window.location.hash = 'this_is_a_hash';
+      result = hashy();
+      return expect(result).toEqual('#this_is_a_hash');
+    });
+    it('sets the hash', function() {
+      var result;
+      hashy('this_is_a_hash');
+      result = hashy();
+      return expect(result).toEqual('#this_is_a_hash');
+    });
+    it('sets an empty hash with one k,v pair', function() {
       var result;
       hashy('key', 'value');
-      result = window.location.hash;
+      result = hashy();
       return expect(result).toEqual('#key=value');
+    });
+    it('sets an empty hash with multiple k,v pairs', function() {
+      var result;
+      hashy(['some', 'pair'], ['key', 'value'], ['test', 'cool']);
+      result = hashy();
+      return expect(result).toEqual('#some=pair&key=value&test=cool');
     });
     it('updates an existing k,v pair in the hash', function() {
       var result;
       hashy('key', 'value');
       hashy('key', 'upated_value');
-      result = window.location.hash;
+      result = hashy();
       return expect(result).toEqual('#key=upated_value');
     });
     it('adds a k,v pair to an existing hash', function() {
       var result;
       hashy('key', 'value');
       hashy('key_1', 'value');
-      result = window.location.hash;
+      result = hashy();
       return expect(result).toEqual('#key=value&key_1=value');
     });
     it('updates an existing k,v pair within a hash containing multiple k,v pairs', function() {
       var result;
       hashy(['some', 'pair'], ['key', 'value'], ['test', 'cool']);
       hashy('key', 'upated_value');
-      result = window.location.hash;
+      result = hashy();
       return expect(result).toEqual('#some=pair&key=upated_value&test=cool');
     });
     return it('creates a new hash containing multiple k,v pairs', function() {
       var result;
       hashy(['some', 'pair'], ['key', 'value'], ['test', 'cool']);
-      result = window.location.hash;
+      result = hashy();
       return expect(result).toEqual('#some=pair&key=value&test=cool');
     });
   });
